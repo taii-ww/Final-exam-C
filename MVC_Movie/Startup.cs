734e4229673c0129d2c6e6e2cdc6c15e62b044b4
taii-ww <tait91026@gmail.com>
2026-05-12 20:36:35 +0700

    project setup

---
+++ MVC_Movie/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MVC_Movie.Startup))]
+
+namespace MVC_Movie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
