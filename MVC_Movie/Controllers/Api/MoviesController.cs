ad6b16fd7693837ecba449e4be3ea14b926c021d
datpham1705 <quocdatpham128@gmail.com>
2026-05-16 00:27:40 +0700

    Add files via upload

---
+++ MVC_Movie/Controllers/Api/MoviesController.cs
@@ -0,0 +1,98 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using MVC_Movie.Dtos;
+using MVC_Movie.Models;
+
+namespace MVC_Movie.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private AppDbContext _appDbContext;
+
+        public MoviesController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        //GET /api/movies
+        [HttpGet]
+        public IEnumerable<MovieDto> GetMovies(string query = null)
+        {
+            var moviesQuery = _appDbContext.Movies
+                   .Include(m => m.Genre)
+                   .Where(m => m.NumberAvailable > 0);
+            if (!string.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+
+            return moviesQuery
+                .ToList()
+                .Select(Mapper.Map<Movie, MovieDto>);
+        }
+
+        //GET /api/movies/1
+        [HttpGet]
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _appDbContext.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        //POST /api/movies
+        [HttpPost]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+            _appDbContext.Movies.Add(movie);
+            _appDbContext.SaveChanges();
+
+            movieDto.Id = movie.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+        }
+
+        //PUT /api/movies/1
+        [HttpPut]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public void UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var movieInDb = _appDbContext.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(movieDto, movieInDb);
+
+            _appDbContext.SaveChanges();
+        }
+
+        //DELETE /api/movies/1
+        [HttpDelete]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public void DeleteMovie(int id)
+        {
+            var movieInDb = _appDbContext.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _appDbContext.Movies.Remove(movieInDb);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
