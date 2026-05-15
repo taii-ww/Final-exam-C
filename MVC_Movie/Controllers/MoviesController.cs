730f1e9a71fdcc970b9dce8c5c700dbfdda03d14
datpham1705 <quocdatpham128@gmail.com>
2026-05-15 22:31:00 +0700

    add code

---
+++ MVC_Movie/Controllers/MoviesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using MVC_Movie.Models;
+using MVC_Movie.ViewModels;
+using System.IO;
+using System.Web;
+
+namespace MVC_Movie.Controllers
+{
+    [AllowAnonymous]
+    public class MoviesController : Controller
+    {
+        private AppDbContext _appDbContext;
+
+        public MoviesController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _appDbContext.Dispose();
+        }
+
+        public ActionResult Random()
+        {
+            //adding data
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "cust1"},
+                new Customer {Name = "cust2"}
+            };
+
+            //filling viewmodel with data
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //passing data do view
+            ViewData["Movie"] = movie;
+            ViewBag.Movie = movie;
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var movie = _appDbContext.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel(movie)
+            {
+                Genres = _appDbContext.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public ActionResult New()
+        {
+            var genres = _appDbContext.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Movie movie, HttpPostedFileBase imageFile)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _appDbContext.Genres.ToList()
+                };
+                return View("MovieForm", viewModel);
+            }
+
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                movie.NumberAvailable = (byte)movie.NumberInStock;
+
+                if (imageFile != null && imageFile.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(imageFile.FileName);
+                    var savePath = Path.Combine(Server.MapPath("~/Content/images/movies"), fileName);
+                    Directory.CreateDirectory(Server.MapPath("~/Content/images/movies"));
+                    imageFile.SaveAs(savePath);
+                    movie.ImagePath = "/Content/images/movies/" + fileName;
+                }
+
+                _appDbContext.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _appDbContext.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.Description = movie.Description;
+
+                if (imageFile != null && imageFile.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(imageFile.FileName);
+                    var savePath = Path.Combine(Server.MapPath("~/Content/images/movies"), fileName);
+                    Directory.CreateDirectory(Server.MapPath("~/Content/images/movies"));
+                    imageFile.SaveAs(savePath);
+                    movieInDb.ImagePath = "/Content/images/movies/" + fileName;
+                }
+            }
+
+            _appDbContext.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
+
+        // Xóa [Authorize(Roles = RoleName.CanManageMovies)] trước Index
+        public ViewResult Index(int? pageIndex, string sortBy)
+        {
+            if (User.IsInRole(RoleName.CanManageMovies))
+                return View("List");        // Admin thấy List (có Edit/Add)
+
+            return View("ReadOnlyList");    // User thường chỉ xem
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _appDbContext.Movies.Include(m => m.Genre).Single(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        public ActionResult Parameters(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(string.Format("PageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year:regex(\\d{4}:range(1800,2021))}/{month:regex(\\d{2}:range(1,12))}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
