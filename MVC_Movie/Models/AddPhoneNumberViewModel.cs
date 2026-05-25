08656b95126f0ac9b7e6b8b21666c96d84c1d477
taii-ww <tait91026@gmail.com>
2026-05-26 00:18:56 +0700

    thêm models quản lý thông tin và bảo mật

---
+++ MVC_Movie/Models/AddPhoneNumberViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Movie.Models
+{
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string Number { get; set; }
+    }
+}
---
+++ MVC_Movie/Models/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+
+namespace MVC_Movie.Models
+{
+    public class IndexViewModel
+    {
+        public bool HasPassword { get; set; }
+        public IList<UserLoginInfo> Logins { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool TwoFactor { get; set; }
+        public bool BrowserRemembered { get; set; }
+    }
+}
