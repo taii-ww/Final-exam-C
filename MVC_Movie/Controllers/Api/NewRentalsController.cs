6ab90814d904948fbbf43888ba3d63a9ca96847a
vinhztrum <vinhyhan@gmail.com>
2026-05-25 23:08:24 +0700

    Qly thue

---
+++ MVC_Movie/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using MVC_Movie.Dtos;
+using MVC_Movie.Models;
+
+namespace MVC_Movie.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private AppDbContext _appDbContext;
+
+        public NewRentalsController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _appDbContext.Customers.SingleOrDefault(
+                c => c.Id == newRental.CustomerId);
+
+            if (customer == null)
+                return BadRequest("CustomerId is not valid.");
+
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No Movie Ids have been given.");
+
+            var movies = _appDbContext.Movies.Where(
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or more MovieIds are invalid.");
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental()
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now,
+                     ScheduledRentalDate = newRental.ScheduledRentalDate,  // Thêm dòng này
+                    ScheduledReturnDate = newRental.ScheduledReturnDate   // Thêm dòng này
+                };
+
+                _appDbContext.Rentals.Add(rental);
+            }
+
+            _appDbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ MVC_Movie/Controllers/RentalsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MVC_Movie.Models; // Thêm dòng này
+
+namespace MVC_Movie.Controllers
+{
+    [Authorize]
+    public class RentalsController : Controller
+    {
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+
+        public ActionResult Book(int id)
+        {
+            var context = new AppDbContext();
+            var movie = context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            ViewBag.MovieId = id;
+            ViewBag.MovieName = movie.Name;
+            return View();
+        }
+    }
+}
