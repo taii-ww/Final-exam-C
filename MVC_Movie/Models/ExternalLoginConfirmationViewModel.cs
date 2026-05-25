63bb287e9e9c636ca417d971229e58a839960b7f
taii-ww <tait91026@gmail.com>
2026-05-26 00:14:32 +0700

    thêm xác thức và quản lý tài khoản

---
+++ MVC_Movie/Models/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Movie.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+    }
+}
---
+++ MVC_Movie/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Movie.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
